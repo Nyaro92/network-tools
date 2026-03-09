a622bcae552a56be33ff80429629bf759ebbbd67
ny4ro <david31.john31@gmail.com>
2026-03-09 13:08:21 +0100

    final du projet v1

--- check.py
+++ check.py
@@ -1,9 +1,7 @@
 import subprocess 
 import platform
 import os
-from urllib.request import build_opener, install_opener
 import speedtest
-from flask import jsonify
 """
 #ping avec subprocess
 def ping_subprocess():
@@ -19,86 +17,36 @@ def ping_subprocess():
     print(lancement)
 """
 
-#on doit utiliser subprocess car os.system donne juste des codes de retour 0 ou 1
+
 def ping_os(packets, host):
-    try:
-        #pour le ping , tester le dns et revoir le chemin de nos traffic 
-        #commande_ping=f"ping -c {packets} {host}"
-        commande_ping=['ping', '-c', str(packets), host]
-        commande_dns=['nslookup', host]
-        commande_traceroute= ['traceroute', host]
-        #requete_ping=os.system(commande_ping)
-        #on utlise subprocess pour attraper les affichages text , en ajoute decode('utf-8) pour rendre lisibles les octets
-        requete_ping=subprocess.check_output(commande_ping,stderr=subprocess.STDOUT).decode('utf-8')
-        #requete_dns=os.system(commande_dns)
-        requete_dns=subprocess.check_output(commande_dns, stderr=subprocess.STDOUT).decode('utf-8')
-        #requete_traceroute= os.system(commande_traceroute)
-        requete_traceroute=subprocess.check_output(commande_traceroute, stderr=subprocess.STDOUT).decode('utf-8')
-        print(requete_ping)
-        print(requete_dns)
-        print(requete_traceroute)
-    # Réunir tous les résultat 
-        final_result=f"----> RÉSULTAT PING\n {requete_ping} \n\n ----> RÉSULTAT DNS\n {requete_dns}\n\n ----> RÉULTAT DE TRACEROUTE\n {requete_traceroute}"
-        return {
-            'requete_ping':final_result
-        }
-    except subprocess.CalledProcessError as e:
-        erreur = e.output.decode('utf-8')
-        if "Name or service not known" in erreur:
-            message = "❌ Domaine invalide ou introuvable."
-        elif "100% packet loss" in erreur:
-            message = "❌ Hôte inaccessible (100% packet loss)."
-        elif "Network is unreachable" in erreur:
-            message = "❌ Réseau inaccessible."
-        else:
-            message = "❌ Erreur lors du ping."
-
-        return {"error": message}
-
-    except Exception as e:
-        return {"error": f"Erreur inattendue : {str(e)}"}
-
+    #pour le ping , tester le dns et revoir le chemin de nos traffic 
+    commande_ping=f"ping -c {packets} {host}"
+    commande_dns=f"nslookup {host}"
+    commande_traceroute= f" traceroute  {host}"
+    
+    requete_ping=os.system(commande_ping)
+    requete_dns=os.system(commande_dns)
+    requete_traceroute= os.system(commande_traceroute)
+    print("ito le an'ilay ping_os")
+    print(requete_ping)
+    print(requete_dns)
+    print(requete_traceroute)
 #elle combine les 3  le ping + traceroutes + latence 
 def mtr_os(host):
     #--report ou -rw pour reporter les analyses
-    #commande_mtr=f"mtr --report {host}"
-    commande_mtr = ["mtr", "--report", "--report-cycles", "1", host]
-    requete_mtr=subprocess.check_output(commande_mtr, stderr=subprocess.STDOUT).decode('utf-8')
+    commande_mtr=f"mtr --report {host}"
+    requete_mtr=os.system(commande_mtr)
 
     print(requete_mtr)
-    return {
-        'requete_mtr':requete_mtr
-    }
+
+
 def bande_passante():
-    try:
-        opener = build_opener()
-        opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-        install_opener(opener)
-
-        st = speedtest.Speedtest(secure=True)
-        st.get_best_server()
-        
-        download = st.download() / 1_000_000
-        upload = st.upload() / 1_000_000
-
-        if download<5:
-            observation="CONNEXION : TRÈS LENTE"
-        elif 5<=download<10:
-            observation="CONNEXION : LENTE"
-        elif 10<=download<50:
-            observation="CONNEXION : BONNE"
-        elif download>=50 :
-            observation="CONNEXION : EXCELLENTE"
-        # ON ENLEVE LES COMMENTAIRES ICI :
-        return {
-            'download': round(download, 2),
-            'upload' : round(upload, 2),
-            'observation':observation
-        }
-    except Exception as e:
-        return {'error': str(e)}
-
-    except subprocess.CalledProcessError as e:
-        return {
-            "error": "❌ Impossible d'exécuter MTR. Hôte invalide ou inaccessible."
-        }
+    print("bonjour dans notre testeur de connexion!!!!!")
+    
+    rq=speedtest.Speedtest()
+    rq.get_best_server()
+    download=rq.download()
+    print(download)
+ 
+    upload=rq.upload()
+    print(upload)
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template,request
 import check
-from flask import jsonify
 
 app= Flask(__name__)
 @app.route("/")
@@ -8,29 +7,21 @@ def principe():
    return render_template("check.html")
 @app.route("/ping", methods=['GET', 'POST'])
 def ping():
-    if request.method == 'GET':
-        return render_template('bande.html')
-    result = None
-    action=request.form.get("action")
-    if request.method == "POST":
-        host = request.form.get("lien")
-        packets = request.form.get("paquets") or 4
-        if action == "btn_pingeo":
-            print("action reçu: ", action )
-            return jsonify(check.ping_os(packets, host))
-        
-        elif action == "btn_mtr":
-            return jsonify(check.mtr_os(host))
-        else:
-            return jsonify({"erreur : action pas trouvé "})
+   result=None
+   host=request.form.get("lien")
+   packets=request.form.get("paquets")
+   if request.form.get("action") == "btn_pingeo":
+      result=check.ping_os(packets, host)
+      return render_template("check.html", name= result)
+   elif request.form.get("action") == "btn_mtr":
+      result=check.mtr_os(host)
+      return render_template("check.html", name=result)
 
 
-
-@app.route("/tester_vitesse", methods=['GET', 'POST'])
+@app.route("/connexion", methods=['GET', 'POST'])
 def bande_passante():
-   # On appelle la fonction et on récupère le dictionnaire
-   donnees = check.bande_passante()
-   # On le transforme en JSON pour le JavaScript
-   return jsonify(donnees)
+   lancement=check.bande_passante()
+   return render_template("check.html", fct=lancement)
+
 if __name__== "__main__":
    app.run(debug=True,host="127.0.0.1", port=5000)
